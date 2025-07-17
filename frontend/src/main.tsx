a1365e25136d728a14cc5088425679fce3013dd3
sigallage <sgallage0000@gmail.com>
2025-07-17 16:45:09 +0530

    updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router}  from 'react-router-dom'
 import AppRoutes from './AppRoutes'
+import React from 'react'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
-  </StrictMode>,
+  </React.StrictMode>,
 )
