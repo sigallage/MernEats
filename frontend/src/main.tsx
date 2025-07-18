ef70eb1069392dd2dfe82ef8ac80fdc19951171c
sigallage <sgallage0000@gmail.com>
2025-07-18 14:37:53 +0530

    create user request

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,25 @@ import { BrowserRouter as Router}  from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import React from 'react'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <QueryClientProvider client={queryClient}>
       <Auth0ProviderWithNavigate>
         <AppRoutes />
       </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
