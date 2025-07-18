f51fdbaf5b27cc746c95f5308fcee41ca2bc97df
sigallage <sgallage0000@gmail.com>
2025-07-18 10:56:11 +0530

    user API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/my/user", MyUserRoute);
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
